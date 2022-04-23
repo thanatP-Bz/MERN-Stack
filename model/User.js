126fde1475fbfdfc9429038b0c5cc3e9e76d5bce
Thanat <bztzn512@gmail.com>
2022-04-23 18:46:16 -0400

    tracking

--- model/User.js
+++ model/User.js
@@ -3,6 +3,35 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+    unique: true,
+  },
+  lastName: {
+    type: String,
+    minlength: 6,
+    maxlength: 20,
+    trim: true,
+    default: "last name",
+  },
+  location: {
+    type: String,
+    minlength: 6,
+    maxlength: 20,
+    trim: true,
+    default: "location",
   },
 });
 
