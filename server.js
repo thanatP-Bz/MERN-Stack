b53bb17b46b010a3e4c6b8b5d1b913547651f55f
Thanat <bztzn512@gmail.com>
2022-05-02 20:34:37 -0400

    proxyAndCors

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ import errorHandlerMiddleware from "./middleware/error-handler.js";
 //parse json
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome! sever side");
+app.get("/api2", (req, res) => {
+  res.json({ smg: "Welcome! sever side" });
 });
 
 //routes
